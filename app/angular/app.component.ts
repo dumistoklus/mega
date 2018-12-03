34b99319b31c81977bb56b15f0266a95954d44dc
goldfarb <goldfarb@yandex-team.ru>
2018-12-03 13:33:16 +0300

    terser vs uglify

--- app/angular/app.component.ts
+++ app/angular/app.component.ts
@@ -3,7 +3,7 @@ import './app.component.css';
 
 @Component({
     selector: '#angular-root',
-    template: `<div><h2 class="angular-title">{{title}}</h2></div>`,
+    template: `<div><h2 class="angular-title">{{title}}</h2><button>Click me!</button></div>`,
 })
 export class AppComponent {
     public title = 'Hello from angular';
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,107 @@
 const { resolve } = require('path');
-const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserJsPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-const config = {
-    stats: {
-        maxModules: 0
-    },
-    mode: 'development',
+module.exports = (env) => {
+    const mode = env && env.production ? 'production' : 'development';
+    const useTerser = env && env.terser;
+    const options = {
+        warnings: false,
+        parse: {
+            html5_comments: false
+        },
+        compress: {
+            hoist_funs: true,
+            passes: 5
+        },
+        output: {
+            comments: false
+        },
+        mangle: true,
+    };
 
-    entry: './main.jsx',
+    if (useTerser) {
+        options.compress.ecma = 5;
+    }
 
-    output: {
-        filename: 'main.js',
-        path: resolve(__dirname, 'dist'),
-        publicPath: '/dist',
-    },
+    const minificator = useTerser ? new TerserJsPlugin({
+        terserOptions: options
+    }) : new UglifyJsPlugin({
+        uglifyOptions: options
+    });
 
-    context: resolve(__dirname, 'app'),
+    return {
+        stats: {
+            maxModules: 0
+        },
 
-    devServer: {
-        contentBase: resolve(__dirname, 'public'),
-        publicPath: '/dist',
-    },
+        mode: mode,
 
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts'],
-    },
+        entry: './main.jsx',
 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: 'awesome-typescript-loader',
-                        options: { configFileName: resolve(__dirname, 'tsconfig.json') }
-                    },
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.jsx?$/,
-                loaders: [
-                    'babel-loader',
-                ],
-                exclude: /node_modules/,
-            },
+        output: {
+            filename: 'main.js',
+            path: resolve(__dirname, 'dist'),
+            publicPath: '/dist',
+        },
+
+        context: resolve(__dirname, 'app'),
+
+        devServer: {
+            contentBase: resolve(__dirname, '.'),
+            publicPath: '/dist',
+        },
+
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts'],
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: 'awesome-typescript-loader',
+                            options: { configFileName: resolve(__dirname, 'tsconfig.json') }
+                        },
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                {
+                    test: /\.jsx?$/,
+                    loaders: [
+                        'babel-loader',
+                    ],
+                    exclude: /node_modules/,
+                },
+            ]
+        },
+
+        optimization: {
+            minimize: true,
+            minimizer: [minificator]
+        },
+
+        plugins: [
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'styles.css'
+            }),
+            new CompressionPlugin()
         ]
-    },
-
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new VueLoaderPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css'
-        })
-    ]
+    };
 };
-
-module.exports = config;
